a13149b3b7aac6bae05a3e6704920388b2c38f46
Ibtihaj <36j207@utas.edu.om>
2026-07-28 00:01:18 +0400

    ERD+MAPPING

---
+++ E-CommerceDatabase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
